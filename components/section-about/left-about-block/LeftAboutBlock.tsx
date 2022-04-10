909ae4d3df0756f3a57582ee456a67e93ea7a5dc
Anton <frontendevds@gmail.com>
2022-04-10 17:06:39 +0300

    added data to about component

--- components/section-about/left-about-block/LeftAboutBlock.tsx
+++ components/section-about/left-about-block/LeftAboutBlock.tsx
@@ -1,15 +1,16 @@
 import styles from './left-about-block.module.css';
 import { Avatar } from './Avatar';
+// Hook
+import { useLanguageState } from '@Hooks/useLanguageState';
+
 export const LeftAboutBlock = () => {
+  const { data } = useLanguageState();
+
   return (
     <article className={styles.module}>
-      <h2 className={styles.title}>Inna Klimenko</h2>
-      <p className={styles.proff}>Водитель, Предприниматель</p>
-      <p className={`${styles.paragraph} paragraph`}>
-        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequat duis enim velit mollit. Exercitation
-        veniam consequat sunt nostrud amet.
-      </p>
+      <h2 className={styles.title}>{data?.about.name}</h2>
+      <p className={styles.proff}>{data?.about.proff}</p>
+      <p className={`${styles.paragraph} paragraph`}>{data?.about.aboutMe}</p>
       <div className={styles.avatar}>
         <Avatar />
       </div>
